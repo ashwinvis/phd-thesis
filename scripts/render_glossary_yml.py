e7a91ab31540f3d0b98dce7d7cdaf235f9645a76
Ashwin Vishnu <ashwinvis+gh@protonmail.com>
2019-08-06 14:15:18 +0200

    Glossary and acronyms

--- scripts/render_glossary_yml.py
+++ scripts/render_glossary_yml.py
@@ -15,6 +15,57 @@ from pathlib import Path
 from render_paper_yml import get_template, render_template
 
 
+GREEK = [
+    "Alpha",
+    "Beta",
+    "Gamma",
+    "Delta",
+    "Epsilon",
+    "Zeta",
+    "Eta",
+    "Theta",
+    "Iota",
+    "Kappa",
+    "Lamda",
+    "Mu",
+    "Nu",
+    "Xi",
+    "Omicron",
+    "Pi",
+    "Rho",
+    "Sigma",
+    "Tau",
+    "Upsilon",
+    "Phi",
+    "Chi",
+    "Psi",
+    "Omega",
+    "alpha",
+    "beta",
+    "gamma",
+    "delta",
+    "epsilon",
+    "zeta",
+    "eta",
+    "theta",
+    "iota",
+    "kappa",
+    "lamda",
+    "mu",
+    "nu",
+    "xi",
+    "omicron",
+    "pi",
+    "rho",
+    "sigma",
+    "tau",
+    "upsilon",
+    "phi",
+    "chi",
+    "psi",
+    "omega",
+]
+
 
 def get_options_from_yaml(path):
     """Read a options YAML file."""
@@ -25,17 +76,26 @@ def get_options_from_yaml(path):
         options = yml.load(f.read())
 
     # post-process options
-    post_options = []
-    for label, description in sorted(options.items()):
-        if label.startswith("text_"):
-            label = name = label.lstrip('text_')
+    glossary = []
+    acronym = []
+    for label, description in options.items():
+        if label.startswith("acr_"):
+            label = name = label.replace("acr_", "")
+            acronym.append((label, name, description))
         else:
-            name = f'${label}$'
-            label = label.lstrip('\\').lstrip('mathbf')
+            name = f"${label}$"
+            for char in r"\{^},":
+                label = label.replace(char, "")
+            for word in ("mathbf", "mathcal", "hat"):
+                if word in label:
+                    label = label.replace(word, "") + "_" + word
+            if any(label.startswith(greek_alphabet) for greek_alphabet in
+                   GREEK):
+                label = "aa_" + label
 
-        post_options.append((label, name, description))
+            glossary.append((label, name, description))
 
-    return post_options
+    return sorted(glossary), sorted(acronym)
 
 
 if __name__ == "__main__":
@@ -50,8 +110,10 @@ if __name__ == "__main__":
         options_file = Path(argv[1])
         template_file = Path(argv[2])
 
-    options = get_options_from_yaml(options_file)
+    glossary, acronym = get_options_from_yaml(options_file)
     template = get_template(template_file)
 
     out_file = os.fspath(options_file.parent / options_file.stem) + ".tex"
-    render_template(template, {"entries": options}, out_file)
+    render_template(
+        template, {"glossary": glossary, "acronym": acronym}, out_file
+    )
